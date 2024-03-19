91d2a894458df60a8676f9e8dd95a9b34709d07b
1337haxorguy <1337haxorguy1@gmail.com>
2024-03-18 20:46:19 -0400

    wow

--- spchk.c
+++ spchk.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <errno.h>  // Include errno.h for error handling
+#include <errno.h>
 #include <fcntl.h>
 #include <dirent.h>
 #include <unistd.h>
